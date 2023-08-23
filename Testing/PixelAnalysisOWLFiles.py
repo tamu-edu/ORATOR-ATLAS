97a3c22800dda1311e25b9ec9507c25bc2afa02e
Anant Bhamri <anantbhamri@tamu.edu>
2023-08-23 16:54:54 -0500

    data analysis of the converted datasets

--- Testing/PixelAnalysisOWLFiles.py
+++ Testing/PixelAnalysisOWLFiles.py
@@ -4,26 +4,39 @@ import json
 import os
 import tkinter as tk
 from tkinter import simpledialog
+from datetime import datetime
+
 
 if __name__ == '__main__':
 
     #parameters
-    input_owl_directory = "../Datasets/Rellis_3D_image_example/img_owl/"
-    owl_mappings_directory = "../Mappings_OWL/"
+    input_owl_directory = "E:/datasets/OWL_datasets/a2d2/"
+    # input_owl_directory = "../Datasets/Rellis_3D_image_example/img_owl/"
+
+    start_time = time.time()
+    now = datetime.now()
+    current_time = now.strftime("%H:%M:%S")
+    print("Current Time =", current_time)
+
     #start of main script
     file_list_owl=[]
+
     for file in os.listdir(input_owl_directory):
         if file.endswith(".owl"):
             temp_file_owl=os.path.join(input_owl_directory, file)
             file_list_owl.append(temp_file_owl)
             #print(temp_file_owl)
 
+    counter = 0
     PixelsPerClass_fullDataSet = {}
+    print("Number of Images", len(file_list_owl))
+
     for input_owl in file_list_owl:
         #load owl ontology
         onto = get_ontology(input_owl)
         onto.load()
-
+        counter = counter + 1
+        print(counter)
         PixelsPerClass = {}
 
         imageName = onto.imaging_conditions.ImageName
@@ -49,4 +62,6 @@ if __name__ == '__main__':
 
        
     print(PixelsPerClass_fullDataSet)
+
+    print("--- %s seconds ---" % (time.time() - start_time))
     print("done")
